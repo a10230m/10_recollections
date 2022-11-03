ddb2af9fc2f08ce380b9351d60d15b2fb84f543d
a10230m <a10230m@gamil.com>
2022-11-03 12:20:10 +0000

    seed README UPDATE

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
   Admin.create!(
-    email: 'a10230m@gmail.com', password: '01978701m'
+    email: 'a10230m@gmail.com', password: '000111'
+  )
+
+
+  users = User.create!(
+    [
+      {email: 'mio@test.com', name: 'mio', password: '111111', birthdate: '1978/12/18', introduction: '趣味は映画・ドラマ鑑賞です。', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+      {email: 'yuto@test.com', name: 'yuto', password: '222222', birthdate: '1989/10/20', introduction: '趣味は旅行です。', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+      {email: 'araki@test.com', name: 'araki', password: '333333', birthdate: '1991/11/22', introduction: '好きなスポーツはバレーボールです。', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")},
+    ]
   )
