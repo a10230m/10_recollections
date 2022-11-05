248476c999bdd09fde7f473015057007ae324402
a10230m <a10230m@gamil.com>
2022-11-05 11:35:38 +0000

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-  email: 'a10230m@gmail.com', password: '000111'
-)
+
+if Admin.find_by(email: 'a10230m@gmail.com').nil?
+  Admin.create!(email: 'a10230m@gmail.com', password: '000111', password_confirmation: '000111')
+end
+
+
 
 
 users =
@@ -17,13 +20,21 @@ users =
     {email: 'araki@test.com', name: 'araki', password: '333333', birthdate: '1991/11/22', introduction: '趣味はものづくり♪台湾が好き！', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")},
     {email: 'mami@test.com', name: 'mami', password: '444444', birthdate: '1987/03/10', introduction: '沖縄大好き！おしゃれなところを探すのが得意。', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user4.jpg"), filename:"sample-user4.jpg")},
     {email: 'tomo@test.com', name: 'tomo', password: '555555', birthdate: '1990/09/15', introduction: '動物が好き。最近ハマっていることはラーメン巡り…', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user5.jpg"), filename:"sample-user5.jpg")}
-  ].map do |user|
-    User.find_or_create_by(user)
+  ].map do |user_attributes|
+    user = User.find_by(email: user_attributes[:email])
+    if user.nil?
+      User.create!(user_attributes)
+    end
+    user
   end
+
 
 # photo
   [
     {photo_title: 'ピアス', photo_caption: '手作り体験してきた！', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3-photo1.jpg"), filename:"sample-user3-photo1.jpg"), user_id: users[2].id },
   ].each do |image|
-    PhotoImage.find_or_create_by(image)
+
+    if PhotoImage.find_by(photo_title: image[:photo_title]).nil?
+      PhotoImage.create!(image)
+    end
   end
